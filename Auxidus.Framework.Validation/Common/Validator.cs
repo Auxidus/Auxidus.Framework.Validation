512bcc944a8fa8421de3b8117e46f5e37364b755
Mitchell Marsh <mmarsh@scale7.co>
2019-04-14 06:20:28 -0700

    Added static Visition common class. Added static throw helper method;

---
+++ Auxidus.Framework.Validation/Common/Validator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auxidus.Framework.Validation.Common
+{
+    public static class Validator
+    {
+        public static void ThrowBrokenRules(IEnumerable<string> brokenRules, string message = null)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                message = "Failed Validation";
+            }
+            List<Exception> validationExceptions = new List<Exception>();
+            brokenRules.ToList().ForEach(e => validationExceptions.Add(new Exception(e)));
+
+            if (validationExceptions.Any())
+            {
+                throw new AggregateException(message, validationExceptions);
+            }
+        }
+
+    }
+}
--- Auxidus.Framework.Validation/Visitor/IValidatable.cs
+++ Auxidus.Framework.Validation/Visitor/IValidatable.cs
@@ -8,4 +8,9 @@ namespace Auxidus.Framework.Validation.Visitor
     {
         bool Validate(IValidator<T> validator, out IEnumerable<string> brokenRules);
     }
+
+    public interface IValidatable
+    {
+        bool Validate<T>(IValidator<T> validator, out IEnumerable<string> brokenRules);
+    }
 }
